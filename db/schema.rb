95bc56e938d0a003dd2eb48865c1ef643e7c3f57
Ritamma <rita07041998@gmail.com>
2023-04-12 22:34:01 +0300

    Add MVC for services

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_114818) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_090728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "phone"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "duration"
+    t.integer "price"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_services_on_company_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_114818) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "services", "companies"
 end
