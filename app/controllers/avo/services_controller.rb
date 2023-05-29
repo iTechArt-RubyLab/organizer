1ced1d1dfd4d9da9a87c455fb1f19c07b2f42dba
Ritamma <rita07041998@gmail.com>
2023-05-29 22:12:45 +0300

    Add views for booking

--- app/controllers/avo/services_controller.rb
+++ app/controllers/avo/services_controller.rb
@@ -1,4 +1,12 @@
 module Avo
   class ServicesController < Avo::ResourcesController
+    around_action :skip_bullet
+
+    def skip_bullet
+      Bullet.enable = false
+      yield
+    ensure
+      Bullet.enable = true
+    end
   end
 end
